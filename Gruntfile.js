d3afb298dca702cf86aecc2f2951bc7553189509
Karen Elliott <btdevil@yahoo.co.uk>
2017-03-01 15:24:17 +0000

    base setup

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+﻿module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+
+    grunt.registerTask('default', ['watch']);
+
+}
